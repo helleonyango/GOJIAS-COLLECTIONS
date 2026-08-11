60becb8b7372e93f060c4744e6500bfe7532e6da
hellleonyango <hellenonyango340@gmail.com>
2026-08-11 15:21:01 +0300

    Add Tailor struct

---
+++ backend/models/tailor.go
@@ -0,0 +1,10 @@
+package models
+
+type Tailor struct {
+    Name        string
+    Phone       string
+    Email       string
+    Specialties []string
+    Portfolio   []string
+    Rating      float64
+}
