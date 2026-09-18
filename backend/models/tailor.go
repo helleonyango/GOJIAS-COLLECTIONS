4a680c1de76f11e4e866d99ea461295ee69e65ae
helleonyango <hellenonyango340@gmail.com>
2026-09-18 12:23:34 +0300

    add UUID generation to Tailor model

--- backend/models/tailor.go
+++ backend/models/tailor.go
@@ -2,6 +2,16 @@ package models
 
 import "github.com/google/uuid"
 
+type Tailor struct {
+    ID          string
+    Name        string
+    Phone       string
+    Email       string
+    Specialties []string
+    Portfolio   []string
+    Rating      float64
+}
+
 func NewTailor(name string, phone string, email string) Tailor {
     return Tailor{
         ID:    uuid.New().String(),
